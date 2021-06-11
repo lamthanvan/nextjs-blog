4dac95dbd45f247f6abfbbc5e2bea51126c08854
macbook <vietweb@outlook.com>
2021-06-11 17:01:07 +0700

    test preview

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'ltv'
+const name = 'lamtv'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
