57cf98e45cb7edceef9964bdb2be7a373b5b2e52
macbook <vietweb@outlook.com>
2021-06-12 09:59:14 +0700

    tra tien cho e

--- pages/index.js
+++ pages/index.js
@@ -20,20 +20,21 @@ export default function Home({ allPostsData }) {
 
             {/* Add this <section> tag below the existing <section> tag */}
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
+                <h2 className={utilStyles.headingLg && utilStyles.rainbow}>Trả tiền cho e a Hoàn ơi</h2>
+                <h2 className={utilStyles.headingLg && utilStyles.rainbow}>Trả tiền cho e a Sơn ơi</h2>
+                {/*<ul className={utilStyles.list}>*/}
+                {/*    {allPostsData.map(({ id, date, title }) => (*/}
+                {/*        <li className={utilStyles.listItem} key={id}>*/}
+                {/*            <Link href={`/posts/${id}`}>*/}
+                {/*                <a>{title}</a>*/}
+                {/*            </Link>*/}
+                {/*            <br />*/}
+                {/*            <small className={utilStyles.lightText}>*/}
+                {/*                <Date dateString={date} />*/}
+                {/*            </small>*/}
+                {/*        </li>*/}
+                {/*    ))}*/}
+                {/*</ul>*/}
             </section>
         </Layout>
     )
